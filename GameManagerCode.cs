a8c28cae85d96e32812b233748a0c1162065aed2
JoseEscurra <103553484+JoseEscurra@users.noreply.github.com>
2022-04-21 11:53:00 -0500

    Add files via upload

---
+++ GameManagerCode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerCode : MonoBehaviour
+{
+    public int PuntosTotales {get {return puntosTotales;}}
+    private int puntosTotales;
+    
+    public void SumarPuntos(int puntosASumar)
+    {
+        puntosTotales += puntosASumar + 10;
+        Debug.Log(puntosTotales);
+    }
+}
---
+++ HUD.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HUD : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameManagerCode gameManager;
+    public TextMeshProUGUI puntos;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        puntos.text = gameManager.PuntosTotales.ToString();
+    }
+}
